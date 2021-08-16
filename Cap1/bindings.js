a23394e7b149808cc289d18062d671d7585693b1
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-16 18:22:37 +0200

    Cap 1: Conditionals

--- Cap1/bindings.js
+++ Cap1/bindings.js
@@ -13,5 +13,9 @@ let luigisDebt = 140;
 luigisDebt = luigisDebt - 35;
 console.log(luigisDebt);
  
+//Bindings #3
+let one = 1;
+let two = 2;
 
+console.log(one + two);
 
---
+++ Cap1/conditionals.js
@@ -0,0 +1,10 @@
+//consitionals 
+let num = Number(prompt("Pick a number"));
+
+if(num < 10) {
+  console.log("Small");
+} else if (num < 100) {
+  console.log("Medium");
+}else {
+  console.log("large");
+}
