bc69621326afcdd8eacc30d8addde58533a43a55
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-17 18:37:26 +0200

    Arrow Function final

--- Cap1/arrow-function.js
+++ Cap1/arrow-function.js
@@ -1,4 +1,4 @@
-
+//Exemple #1
 const power = (base, exponent) => {
     let result = 1;
     for (let count = 0; count < exponent; count++){
@@ -8,3 +8,16 @@ const power = (base, exponent) => {
   }
   
   console.log(power)
+
+//Exemple #2
+const square1 = (x) => {return x * x; };
+const square2 = x => x * x;
+
+console.log(square1);
+
+const horn = () => {
+  console.log("Toot");
+}
+
+
+
