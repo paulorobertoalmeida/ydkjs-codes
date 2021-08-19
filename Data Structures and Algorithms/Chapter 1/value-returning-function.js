87aaaf1d733f02c427bbcdeda93c9c7854ecbf75
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-19 19:36:50 +0200

    Chapter 1: repetition Constructs void functions

--- Data Structures and Algorithms/Chapter 1/value-returning-function.js
+++ Data Structures and Algorithms/Chapter 1/value-returning-function.js
@@ -1,4 +1,5 @@
 //A value-returning function
+//This exemple demonstrates how value-returning functions are defined and called in javascript.
 
 function factorial(number){
     let product = 1;
@@ -15,4 +16,22 @@ function factorial(number){
   24
   120
   3628800
+  */
+
+//A subprocedure or void function in javascript
+//this exemple illustrates how write a function in javascript that is used not for its return value, but for the operations it performs.
+
+function curve(arr, amount) {
+    for (let i = 0; i < arr.length; i++){
+      arr[i] += amount;
+    }
+  }
+  
+  let grades = [77, 73, 74, 81, 90];
+  curve(grades, 5);
+  console.log(grades);
+  
+  /* 
+  Result:
+  [ 82, 78, 79, 86, 95 ]
   */
