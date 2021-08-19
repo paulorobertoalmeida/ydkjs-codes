94f15998f16e171b41c49ffffb774f652297b382
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-19 19:46:22 +0200

    Chapter 1: Variable Scope: Exemple 2

--- Data Structures and Algorithms/Chapter 1/variable-scope.js
+++ Data Structures and Algorithms/Chapter 1/variable-scope.js
@@ -16,3 +16,25 @@ function showScope() {
   global
   [Function: showScope]
   */
+
+  //Variable Scope: Declaring Second Variable
+
+function showScope() {
+    //let scope = "local";
+    const scope = "local";
+    //var scope = "local"; 
+    return scope;
+  }
+  
+  //let scope = "global";
+  const scope = "global";
+  //var scope = "global";
+  console.log(scope);
+  console.log(showScope);
+  
+  
+  /* 
+  Result:
+  global
+  [Function: showScope]
+  */
