5e23fc7b7720072ccd1e4a59e646ea632de360d4
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-19 19:05:15 +0200

    Chapter 1: If Else Statement

--- Data Structures and Algorithms/Chapter 1/decision-constructor.js
+++ Data Structures and Algorithms/Chapter 1/decision-constructor.js
@@ -30,3 +30,24 @@ Result:
 6
 31.5
 */
+
+//the if- else if statement
+
+let mid = 25;
+let high = 50;
+let low = 1;
+let current = 13;
+let found = -1;
+if (current < mid) {
+  mid = (current - low) / 2;
+} else if (current > mid) {
+  mid = (current + high) / 2;
+} else {
+  mid = (current + high) / 2;
+}
+console.log(mid);
+
+/* 
+Result:
+6
+*/
