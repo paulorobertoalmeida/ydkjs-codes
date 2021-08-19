baf1ea8678d46a4a63c404797bfc79b41ad60dc5
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-19 19:21:17 +0200

    Chapter 1: repetition Constructs for loop

--- Data Structures and Algorithms/Chapter 1/repetition-constructs.js
+++ Data Structures and Algorithms/Chapter 1/repetition-constructs.js
@@ -10,3 +10,15 @@ console.log(sum);
 Result:
 55
 */
+
+//For loop
+let number = 1;
+let sum = 0;
+for ( let number = 1; number < 11; number++){
+  sum += number;
+}
+console.log(sum);
+/* 
+Result:
+55
+*/
