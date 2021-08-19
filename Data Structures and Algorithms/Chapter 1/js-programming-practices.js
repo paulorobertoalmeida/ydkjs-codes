6d87ad5ae617264c9873c5e7d19325534bddf0fb
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-19 12:12:20 +0200

    Chapter 1:Declaring variables + Arithmetic

---
+++ Data Structures and Algorithms/Chapter 1/js-programming-practices.js
@@ -0,0 +1,29 @@
+//Declaring Variables
+
+let number;
+let name;
+let rate = 1.2;
+let greeting = " Hello Cowboy";
+let flag = false;
+
+
+//Arithmetic and math functions in JS
+
+let x = 3;
+let y = 1.1;
+console.log(x + y);
+console.log(x * y);
+console.log((x+y)*(x-y));
+let z = 9;
+console.log(Math.sqrt(z));
+console.log(Math.abs(y/x));
+
+/* 
+Result:
+4.1
+3.3000000000000003
+7.789999999999999
+3
+0.3666666666666667
+
+*/
