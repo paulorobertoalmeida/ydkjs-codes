b1f105fc1e5459ca3f3e67f9740c942515b15cff
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-20 17:03:44 +0200

    Chapter 2: Arrays Part 1

---
+++ Data Structures and Algorithms/Chapter 2/arrays-part1.js
@@ -0,0 +1,28 @@
+// Arrays
+// Declaring an Arrays
+
+// Simple way to declare an array
+// let numbers = [];
+
+// Declaring an array variable with a set of elements inside
+//let numbers = [1, 2, 3, 4, 5];
+
+//Calling an array Constructor
+//let numbers = new Array();
+
+//Calling an array with set of elements as arguments to the Constructor
+
+let numbers = new Array(1, 2, 3, 4, 5);
+
+console.log(numbers.length);
+
+
+
+
+/* 
+Result:
+0
+5
+0
+5
+*/
