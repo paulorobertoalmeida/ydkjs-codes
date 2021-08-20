a501bb3f9910e6f9036781c0a8f85f43edf2f709
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-20 17:25:45 +0200

    Chapter 2: Acessing and Writing Elements part 2

--- Data Structures and Algorithms/Chapter 2/arrays-part2.js
+++ Data Structures and Algorithms/Chapter 2/arrays-part2.js
@@ -9,14 +9,24 @@ console.log(nums);
 */
 
 // Accesing elements using the [] operator
+/*
 let numbers = [1, 2, 3, 4, 5];
 let sum = numbers[0] + numbers[1] + numbers[2] + numbers      [3] + numbers[4];
 console.log(sum);
+*/
 
+// Accesing an array using for loop
+let numbers = [1, 2, 3, 5, 8, 13, 21];
+let sum = 0;
+for (let i = 0; i < numbers.length; ++i) {
+  sum += numbers[i];
+}
+console.log(sum);
 
 
 /*
 Result:
 [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
 15
+53
 */
