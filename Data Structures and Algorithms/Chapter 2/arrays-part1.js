5b8764f896b81233ae66b7188832e6e3ae375041
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-20 17:13:53 +0200

    Chapter 2: Arrays Part 2

--- Data Structures and Algorithms/Chapter 2/arrays-part1.js
+++ Data Structures and Algorithms/Chapter 2/arrays-part1.js
@@ -11,11 +11,25 @@
 //let numbers = new Array();
 
 //Calling an array with set of elements as arguments to the Constructor
+//let numbers = new Array(1, 2, 3, 4, 5);
 
-let numbers = new Array(1, 2, 3, 4, 5);
+// Declaring an array by calling the Array constructor with a single argument specifying the length of the array
+//let numbers = new Array(10);
 
-console.log(numbers.length);
+//Elements do not all have to be the same type
+//let objects = [1, "Joe", true, null]
+//console.log(objects);
 
+//console.log(numbers.length);
+
+
+
+//Verifying that an object in an array
+
+let numbers = 3;
+let arr = [7, 4, 1776];
+console.log(Array.isArray(numbers));
+console.log(Array.isArray(arr));
 
 
 
@@ -25,4 +39,8 @@ Result:
 5
 0
 5
+10
+[ 1, 'Joe', true, null ]
+false
+true
 */
