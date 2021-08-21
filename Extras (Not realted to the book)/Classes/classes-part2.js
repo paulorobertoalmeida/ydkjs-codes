5df4a644b092e24d3a024a5327fe5273c26ada78
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-21 18:44:29 +0200

    Creating Extras: Classes Part 2 (super)

--- Extras (Not realted to the book)/Classes/classes-part2.js
+++ Extras (Not realted to the book)/Classes/classes-part2.js
@@ -11,3 +11,27 @@ class MinhaArray extends Array {
  
  console.log(mapped instanceof MinhaArray); // false
  console.log(mapped instanceof Array); // true
+
+ 
+ 
+ // Calling Father Class using super
+ // Gato class -> nome parameter -> this.nome property -> falar() method ->
+ class Gato {
+    constructor(nome) {
+       this.nome = nome;
+    }
+ 
+    falar() {
+       console.log(this.nome + ' faça barulho.');
+    }
+ }
+ 
+ class Leao extends Gato {
+    falar() {
+       super.falar();
+       console.log(this.nome + ' roars.');
+    }
+ }
+ 
+ let leao = new Leao('Fuzzy');
+ leao.falar();
