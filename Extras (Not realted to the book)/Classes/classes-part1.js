5ed1fc8e1f31125e11d0fbdac5a11022d2661a72
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-21 18:06:43 +0200

    Creating Extras: Adding some new studies from Mozilla Developer Part 2

---
+++ Extras (Not realted to the book)/Classes/classes-part1.js
@@ -0,0 +1,18 @@
+// Prototype Method
+class Retangulo {
+    constructor(altura, largura) {
+      this.altura = altura; this.largura = largura;
+    }
+  //Getter
+    get area() {
+        return this.calculaArea()
+    }
+
+    calculaArea() {
+        return this.altura * this.largura;
+    }
+}
+
+const quadrado = new Retangulo(10, 10);
+
+console.log(quadrado.area);
