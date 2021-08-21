71813c2103a5a82d66d349bd0e5c552d49390758
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-21 18:15:09 +0200

    Creating Extras: Adding some new studies from Mozilla Developer Part 3

--- Extras (Not realted to the book)/Classes/classes-part1.js
+++ Extras (Not realted to the book)/Classes/classes-part1.js
@@ -16,3 +16,30 @@ class Retangulo {
 const quadrado = new Retangulo(10, 10);
 
 console.log(quadrado.area);
+
+
+//Static Method
+
+class Ponto {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    static distancia(a, b) {
+        const dx = a.x - b.x;
+        const dy = a.y - b.y;
+
+        return Math.hypot(dx, dy);
+    }
+}
+
+const p1 = new Ponto(5, 5);
+const p2 = new Ponto(10, 10);
+
+p1.distancia; //undefined
+p2.distancia; //undefined
+
+console.log(Ponto.distancia(p1, p2));
+console.log(p1);
+console.log(p2);
