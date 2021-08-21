34ea437571432eeced3f94219619bb8a23d8ff3a
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-21 18:50:56 +0200

    Creating Extras: Classes Part 2 (mix-ins)

--- Extras (Not realted to the book)/Classes/classes-part2.js
+++ Extras (Not realted to the book)/Classes/classes-part2.js
@@ -35,3 +35,29 @@ class MinhaArray extends Array {
  
  let leao = new Leao('Fuzzy');
  leao.falar();
+
+ //Mix-ins
+
+class Humano {
+    constructor(nome) {
+      this.nome = nome;
+    }
+    andar() {
+      return this.nome+ ' andou um passo';
+    }
+  }
+  
+  const HumanoFalante = Base => class extends Base {
+    falar() {
+      return this.nome+ ' diz: olá mundo!';
+    }
+  }
+  
+  const HumanoFalanteMixado = Base => class extends Base {}
+  
+  const HumanoFinal = HumanoFalanteMixado(HumanoFalante(Humano));
+  
+  const humano = new HumanoFinal('Bill Gates');
+  
+  console.log(humano.andar());
+  console.log(humano.falar());
