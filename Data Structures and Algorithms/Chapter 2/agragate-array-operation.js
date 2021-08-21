0c11c1c002eb30b0ec5f7bdf8b310143f68fd948
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-21 12:35:29 +0200

    Chapter 2 Arrays: Agregate Array Operation Part 2

--- Data Structures and Algorithms/Chapter 2/agragate-array-operation.js
+++ Data Structures and Algorithms/Chapter 2/agragate-array-operation.js
@@ -1,5 +1,5 @@
 // Agregate Array Operations
-
+/*
 let nums = [];
 for ( let i = 0; i < 100; ++i) {
   nums[i] = i+1;
@@ -8,10 +8,23 @@ for ( let i = 0; i < 100; ++i) {
 let samenums = nums;
 nums[0]= 400;
 console.log(samenums[0]); // this is called shallow copy
-
-
-
-/* 
-Result: 
-400
 */
+function copy(arr1, arr2) {
+    for (let i = 0; i < arr1.length; ++i) {
+      arr2[i] = arr1[i];
+    }
+  }
+  
+  let nums = [];
+  for (let i = 0; i < 100; ++i) {
+    nums[i] = i+1;
+  }
+  let samenums = [];
+  copy(nums, samenums);
+  nums[0] = 400;
+  console.log(samenums[0]);
+  
+  /* 
+  Result: 
+  400
+  */
