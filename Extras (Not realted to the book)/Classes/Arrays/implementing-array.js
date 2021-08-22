de6804246ea6e7f91a296885eaf51513b8091a91
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-22 11:52:05 +0200

    Creating Extras: Implementing an Array Final Part

--- Extras (Not realted to the book)/Classes/Arrays/implementing-array.js
+++ Extras (Not realted to the book)/Classes/Arrays/implementing-array.js
@@ -1,44 +1,46 @@
 // Implementing Array
 
 class MyArray {
-    constructor(){
+    constructor() {
       this.length = 0;
       this.data = {};
     }
     get(index) {
       return this.data[index];
     }
-    push(item){
+    push(item) {
       this.data[this.length] = item;
-      this.length ++;
-      return this.length;
+      this.length++;
+      return this.data;
     }
     pop() {
-      const lastItem = this.data[this.length-1];
-      delete this.data[this.length-1];
+      const lastItem = this.data[this.length - 1];
+      delete this.data[this.length - 1];
       this.length--;
       return lastItem;
     }
-    delete(index) {
+    deleteAtIndex(index) {
       const item = this.data[index];
       this.shiftItems(index);
+      return item;
     }
     shiftItems(index) {
-      for (let i = index; i < this.length -1; i ++ ){
-        this.data[i] = this.data[i+1];
+      for (let i = index; i < this.length - 1; i++) {
+        this.data[i] = this.data[i + 1];
       }
-      delete this.data[this.length-1];
+      console.log(this.data[this.length - 1]);
+      delete this.data[this.length - 1];
       this.length--;
     }
   }
   
-  const newArray = new MyArray();
-  newArray.push("hi");
-  console.log(newArray.get(0));
-  //newArray.push("you");
-  //console.log(newArray);
-/*
-newArray.pop();
-newArray.pop();
-console.log(newArray);
-*/
+  const myArray = new MyArray();
+  myArray.push('hi');
+  myArray.push('you');
+  myArray.push('!');
+  myArray.pop();
+  myArray.deleteAtIndex(0);
+  myArray.push('are');
+  myArray.push('nice');
+  myArray.shiftItems(0);
+  console.log(myArray);
