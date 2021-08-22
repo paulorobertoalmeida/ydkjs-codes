1ae3a9924ca0776d0a05c340e04dc124feac74e8
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-22 11:16:45 +0200

    Creating Extras: Implementing an Array Part 1

---
+++ Extras (Not realted to the book)/Classes/Arrays/implementing-array.js
@@ -0,0 +1,18 @@
+class MyArray {
+    constructor(){
+      this.length = 0;
+      this.data = {};
+    }
+    get(index) {
+      return this.data[index]
+    }
+    push(item){
+      this.data[this.length] = item;
+      this.length ++;
+      return this.length;
+    }
+  }
+  
+  const newArray = new MyArray();
+  newArray.push("hi");
+  console.log(newArray.get(0));
