7ccc1659364572c1b5ae6659982a50df1f52749e
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-24 17:44:14 +0200

    Mutator Function: Using Length

--- Data Structures and Algorithms/Chapter 2/mutator-function.js
+++ Data Structures and Algorithms/Chapter 2/mutator-function.js
@@ -10,3 +10,17 @@ console.log(nums)
 [ 1, 2, 3, 4, 5 ]
 [ 1, 2, 3, 4, 5, 6 ]
 */
+
+// Mutator Function
+// Push - Adds an element to the end of an array
+// Using length property to see that its not so intuitive
+
+let nums = [1, 2, 3, 4, 5];
+console.log(nums);
+nums[nums.length] = 6;
+console.log(nums)
+
+/* 
+[ 1, 2, 3, 4, 5 ]
+[ 1, 2, 3, 4, 5, 6 ]
+*/
