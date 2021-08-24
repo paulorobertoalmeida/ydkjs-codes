1d8a7b84102206c0782ae8110fba5fce13ecca11
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-24 17:10:29 +0200

    Arrays: Searching for a value using indexof() & lastIndexOf()

--- Data Structures and Algorithms/Chapter 2/accesor-functions.js
+++ Data Structures and Algorithms/Chapter 2/accesor-functions.js
@@ -9,3 +9,14 @@ if ( position >= 0) {
 } else {
   console.log(name + " not found in array.");
 }
+
+// Seaching for a value
+
+let names = [ "David","Mike", "Cynthia", "Raymond", "Clayton", "Jennifer"];
+let name = "Mike";
+let firstPos = names.indexOf(name);
+console.log("First found "+ name + " at position " + firstPos);
+let lastPos = names.lastIndexOf(name);
+console.log("Last found "+ name + " at position " + lastPos);
+
+
