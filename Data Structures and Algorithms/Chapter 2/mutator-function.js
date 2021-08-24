d42d07a7cdd06cb8c57da3ad4ad663644eda808b
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-24 17:42:22 +0200

    Mutator Function: Push

---
+++ Data Structures and Algorithms/Chapter 2/mutator-function.js
@@ -0,0 +1,12 @@
+// Mutator Function
+// Push - Adds an element to the end of an array
+
+let nums = [1, 2, 3, 4, 5];
+console.log(nums);
+nums.push(6);
+console.log(nums)
+
+/* 
+[ 1, 2, 3, 4, 5 ]
+[ 1, 2, 3, 4, 5, 6 ]
+*/
