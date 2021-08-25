a2d75c9d5e08391cf3c56ec9e24bf60362111cd3
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-25 11:11:46 +0200

    Code interview 1

---
+++ Extras (Not realted to the book)/Exercises/code-interview-test.js
@@ -0,0 +1,18 @@
+let newArray = [];
+
+for (let i = 0; i < 1000; i++) {
+    newArray[i] = i;
+    newArray[i] = (Math.random());
+
+  
+}
+console.log(newArray);
+
+  if (i > 0.0 &&  i < 0.1){
+    console.log(i + "este numero esta no slot 1");
+  } else if(i > 0.1 &&  i < 0.2){
+    console.log(i + "este numero esta no slot 2");
+  } 
+
+
+
