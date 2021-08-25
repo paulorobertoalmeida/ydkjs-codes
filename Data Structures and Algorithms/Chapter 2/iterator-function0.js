ff472798e5f0e19548fd0d944dd130b3954468c0
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-25 12:56:59 +0200

    Iterator Function ZERO: Part 1

---
+++ Data Structures and Algorithms/Chapter 2/iterator-function0.js
@@ -0,0 +1,23 @@
+// Iterator Function
+
+function square(num) {
+    console.log(num, num * num);
+  }
+  let nums = [1,2,3,4,5,6,7,8,9,10];
+  nums.forEach(square);
+  
+  
+  
+  /* 
+  1 1
+  2 4
+  3 9
+  4 16
+  5 25
+  6 36
+  7 49
+  8 64
+  9 81
+  10 100
+  */
+  
