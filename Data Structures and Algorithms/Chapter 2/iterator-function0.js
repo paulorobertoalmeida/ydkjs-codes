d68bfcb7459c80e90b336bbccf024eee08093a4b
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-25 13:06:26 +0200

    Iterator Function ZERO: Part 2 Even and Odd

--- Data Structures and Algorithms/Chapter 2/iterator-function0.js
+++ Data Structures and Algorithms/Chapter 2/iterator-function0.js
@@ -22,3 +22,31 @@ function square(num) {
   10 100
   */
   
+  // Iterator Function
+// every()
+
+let nums = [1,2,3,4,5,6,7,8,9,10];
+
+function isEven(num) {
+  if (num % 2 == 0) { 
+    console.log("This number is even " + num)
+  } else {
+    console.log("This number is 0dd " + num)
+  }
+}
+
+let even = nums.forEach(isEven);
+
+
+/* 
+This number is 0dd 1
+This number is even 2
+This number is 0dd 3
+This number is even 4
+This number is 0dd 5
+This number is even 6
+This number is 0dd 7
+This number is even 8
+This number is 0dd 9
+This number is even 10
+*/
