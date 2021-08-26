0d316125b8a8e87b42f07adb297ce5af98a569e2
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-26 17:57:23 +0200

    Chapter 2 Arrays: Two-Dimensional Arrays Part 1

---
+++ Data Structures and Algorithms/Chapter 2/twoDimensional-arrays.js
@@ -0,0 +1,17 @@
+// Two-Dimensional Arrays
+
+let two = [];
+let rows = 5;
+for (let i = 0; i < rows; ++i) {
+  two[i] = [];
+}
+
+console.log(two)
+console.log(rows)
+/* 
+Result:
+[ [], [], [], [], [] ]
+5
+*/
+
+
