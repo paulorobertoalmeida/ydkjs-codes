6b3ef3e04b156228834d3586bcf499b593bcca4a
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-26 19:08:15 +0200

    Chapter 2 Arrays: Two-Dimensions Arrays Part 3

--- Data Structures and Algorithms/Chapter 2/twoDimensional-arrays.js
+++ Data Structures and Algorithms/Chapter 2/twoDimensional-arrays.js
@@ -40,3 +40,33 @@ Array.matrix = function(numrows, numcols, initial) {
   Joe
   */
   
+
+  // Two-Dimensional Arrays
+
+let grades = [[89, 77, 78], [76, 82, 81], [91, 94, 89]];
+console.log(grades[2][2])
+/* 
+Result:
+89
+*/
+
+
+// Two-Dimensional Arrays
+
+let grades = [[89, 77, 78], [76, 82, 81], [91, 94, 89]];
+let total = 0;
+let average = 0.0;
+for (let row = 0; row < grades.length; ++row){
+  for (let col = 0; col < grades[row].lenght; ++col){
+    total += grades[row][cow];
+  }
+  average = total * grades[row].length;
+  console.log("Student " + parseInt(row+1) + " average: " + average.toFixed(2));
+  total = 0;
+  average = 0.0;
+}
+
+/* 
+Result:
+
+*/
