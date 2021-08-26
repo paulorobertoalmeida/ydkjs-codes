ebe7d75499a8dfa5c4a594d711b09bad65cc6944
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-26 18:07:31 +0200

    Chapter 2 Arrays: Two-Dimensions Arrays Part 2

--- Data Structures and Algorithms/Chapter 2/twoDimensional-arrays.js
+++ Data Structures and Algorithms/Chapter 2/twoDimensional-arrays.js
@@ -14,4 +14,29 @@ Result:
 5
 */
 
+// Two-Dimensional Arrays
 
+
+Array.matrix = function(numrows, numcols, initial) {
+    let arr = [];
+    for (let i = 0; i < numrows; i++) {
+      let columns = [];
+      for( let j = 0; j < numcols; j++) {
+        columns[j] = initial;
+      }
+      arr[i] = columns;
+    }
+    return arr;
+  }
+  let nums = Array.matrix(5, 5, 0);
+  console.log(nums[1][1]);
+  let names = Array.matrix(3, 3, "");
+  names[1][2]= "Joe";
+  console.log(names[1][2]);
+  
+  /* 
+  Result:
+  0
+  Joe
+  */
+  
