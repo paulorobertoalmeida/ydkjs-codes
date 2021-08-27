528167d11186cf6d2a7d9d6782d635fb6f19028a
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-27 18:42:13 +0200

    Chapter 2 Arrays: Array in Object

---
+++ Data Structures and Algorithms/Chapter 2/array-in-object.js
@@ -0,0 +1,37 @@
+// ARRAYS IN OBJECTS
+
+function weekTemps() {
+    this.dataStore = [];
+    this.add = add;
+    this.average = average;
+  }
+  
+  function add(temp) {
+    this.dataStore.push(temp);
+  }
+  
+  function average() {
+    let total = 0;
+    for (let i = 0; i < this.dataStore.length; i++) {
+      total += this.dataStore[i];
+    }
+    return total / this.dataStore.length;
+  }
+  
+  let thisWeek = new weekTemps();
+    thisWeek.add(52);
+    thisWeek.add(55);
+    thisWeek.add(61);
+    thisWeek.add(65);
+    thisWeek.add(55);
+    thisWeek.add(50);
+    thisWeek.add(52);
+    thisWeek.add(49);
+  
+  console.log(thisWeek.average());
+  
+  
+  /* 
+  Result:
+  54.875
+  */
