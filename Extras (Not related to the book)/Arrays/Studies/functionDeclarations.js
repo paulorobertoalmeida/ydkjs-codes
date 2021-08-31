3231abd38ff86b020d994711be6c2c878a721fda
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-31 19:21:38 +0200

    Studies: Function Declarations Part 3 fixing an error

--- Extras (Not related to the book)/Arrays/Studies/functionDeclarations.js
+++ Extras (Not related to the book)/Arrays/Studies/functionDeclarations.js
@@ -11,4 +11,19 @@ let sayBye = function(){
     console.log("bye");
 }
 
-SayBye();
+sayBye();
+
+//
+function sing(song) {
+    console.log(song);   
+}
+
+sing("lalalalal");
+sing("lililili");
+sing("lololo");
+
+function multiply(a,b) {
+    return a * b;
+}
+
+multiply(5,10);
