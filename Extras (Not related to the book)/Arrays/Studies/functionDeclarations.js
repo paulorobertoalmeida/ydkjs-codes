444bdfc43483b3e5b6a3438c0f252bb8fe0639e9
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-08-31 19:14:27 +0200

    Studies: Function Declarations Part 2

---
+++ Extras (Not related to the book)/Arrays/Studies/functionDeclarations.js
@@ -0,0 +1,14 @@
+// Function Declaration
+function sayHello() {
+    console.log("hello");
+}
+
+sayHello();
+
+// Function Expression
+
+let sayBye = function(){
+    console.log("bye");
+}
+
+SayBye();
