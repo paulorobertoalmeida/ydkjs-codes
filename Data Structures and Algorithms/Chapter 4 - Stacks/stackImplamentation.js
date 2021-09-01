2c250e3884e900bea57d5a81ff089e4c1eeac27f
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-09-01 11:33:05 +0200

    Chapter 4 Stacks: Stack Implemetation Part 3

--- Data Structures and Algorithms/Chapter 4 - Stacks/stackImplamentation.js
+++ Data Structures and Algorithms/Chapter 4 - Stacks/stackImplamentation.js
@@ -11,4 +11,23 @@ function Stack() {
     this.dataStack[this.top++] = element;
   }
   
+  function pop() {
+    return this.dataStack[--this.top];
+  }
+  
+  //      
+  //      2
+  //    1 1
+  
+  function peek() {
+    returns this.dataStack[this.top -1];
+  }
+  
+  //      3*peek
+  //    2 2
+  //  1 1 1
+  
+  function length() {
+    return this.top;
+  }
   
