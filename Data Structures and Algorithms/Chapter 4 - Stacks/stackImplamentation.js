26da83ea3dc4048acd66e58f6bc9e1a80a1da142
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-09-01 11:46:22 +0200

    Chapter 4 Stacks: Stack Implemetation Final

--- Data Structures and Algorithms/Chapter 4 - Stacks/stackImplamentation.js
+++ Data Structures and Algorithms/Chapter 4 - Stacks/stackImplamentation.js
@@ -4,12 +4,27 @@ function Stack() {
     this.dataStack = [];
     this.top = 0;
     this.push = push;
-    this.pop = pop;
     this.peek = peek;
+    this.clear = clear;
+    this.pop = pop;
+    this.length = length;
   }
+  
   function push(element) {
     this.dataStack[this.top++] = element;
   }
+  
+  //      3
+  //    2 2
+  //  1 1 1
+  
+  function peek() {
+    return this.dataStack[this.top-1];
+  }
+  
+  //      3*peek
+  //    2 2
+  //  1 1 1
   
   function pop() {
     return this.dataStack[--this.top];
@@ -19,15 +34,35 @@ function Stack() {
   //      2
   //    1 1
   
-  function peek() {
-    returns this.dataStack[this.top -1];
+  function clear() {
+    this.top = 0;
   }
-  
-  //      3*peek
-  //    2 2
-  //  1 1 1
   
   function length() {
     return this.top;
   }
   
+  let s = new Stack();
+  s.push("David");
+  s.push("Raymond");
+  s.push("Bryan");
+  console.log("length: " +s.length());
+  console.log(s.peek());
+  let popped = s.pop();
+  console.log("The popped element is: " + popped);
+  console.log(s.peek());
+  s.clear();
+  console.log("length: "+ s.length());
+  console.log(s.peek());
+  s.push("Clayton");
+  console.log(s.peek());
+  
+  /*
+  length: 3
+  Bryan
+  The popped element is: Bryan
+  Raymond
+  length: 0
+  undefined
+  Clayton
+   */
