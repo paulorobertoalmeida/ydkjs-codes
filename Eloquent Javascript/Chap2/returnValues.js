1ad75a6e9193a66ab16de73a33ba4df3381e4c17
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-09-04 23:09:30 +0200

    Eloquent Javascript Introduction: Return Values Part 2

--- Eloquent Javascript/Chap2/returnValues.js
+++ Eloquent Javascript/Chap2/returnValues.js
@@ -10,3 +10,29 @@ if (!Number.isNaN(theNumber)) {
 } else {
   console.log("Hey. Why didnt you give me a number?");
 }
+
+let num = Number(prompt("Pick a number"));
+
+if (num < 10) {
+  console.log("Small");
+} else if (num < 100) {
+  console.log("Medium");
+} else {
+  console.log("Large");
+}
+
+//While loop
+
+let number = 0;
+while (number <= 12) {
+  console.log(number);
+  number = number + 2;
+} //0,1,2,3,4,5,6,7,8,9,10,11,12
+
+let result = 1;
+let counter = 0;
+while (counter < 10) {
+  result = result * 2;
+  counter = counter + 1;
+}
+console.log(result); //1024
