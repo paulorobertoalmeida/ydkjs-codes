09bd8ecd009ab1eb8a3734b3793a5a8ae80e9e39
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-09-04 22:52:58 +0200

    Eloquent Javascript Introduction: Bindings

---
+++ Eloquent Javascript/Chap2/bindings.js
@@ -0,0 +1,16 @@
+// Bindings
+let ten = 10;
+console.log(ten * ten); //100
+
+let mood = "light";
+console.log(mood); //light
+mood = "dark";
+console.log(mood); //dark
+
+let one = 1;
+let two = 2;
+console.log(one + two); //3
+
+var name = "Ayda";
+const greeting = "Hello";
+console.log(greeting + " " + name); //Hello Ayda
