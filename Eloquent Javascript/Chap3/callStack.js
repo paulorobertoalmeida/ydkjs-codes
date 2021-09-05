5149da804cff674830782adea8ca839196249a0c
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-09-05 18:34:18 +0200

    Eloquent Javascript Introduction: Closure 5

--- Eloquent Javascript/Chap3/callStack.js
+++ Eloquent Javascript/Chap3/callStack.js
@@ -80,3 +80,18 @@ function findSolution(target) {
   }
   
   console.log(findSolution(24)); //(((1 * 3) + 5) * 3)
+
+
+  function printFarmInventory(cows, chicken) {
+    let cowString = String(cows);
+    while (cowString.length < 3) {
+      cowString = "0" + cowString;
+    }
+    console.log(`${cowString} Cows`);
+    let chickenString = String(chickens);
+    while (chickenString.length < 3) {
+      chickenString = "0" + chickenString;
+    }
+    console.log(`${chickenString} Chickens`);
+  }
+  printFarmInventory(7, 11); //007
