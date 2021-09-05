f13a456b3043989221a668b36507fdba41891bc3
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-09-05 10:52:54 +0200

    Eloquent Javascript Introduction: For Loop 2

--- Eloquent Javascript/Chap2/returnValues.js
+++ Eloquent Javascript/Chap2/returnValues.js
@@ -60,3 +60,23 @@ if (false != true) {
 for (let number = 0; number <= 12; number = number + 2) {
     console.log(number);
   }  
+
+  /*
+  0
+2
+4
+6
+8
+10
+12 
+
+*/
+
+// For Loops
+
+let result = 1;
+for (let counter = 0; counter < 10; counter = counter +1) {
+  result = result * 2;
+}
+
+console.log(result); //1024
