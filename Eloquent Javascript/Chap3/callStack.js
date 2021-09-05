857e1b7aa631d0f2773973cbb3d856d337f8ec76
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-09-05 18:15:53 +0200

    Eloquent Javascript Introduction: Closure 2

--- Eloquent Javascript/Chap3/callStack.js
+++ Eloquent Javascript/Chap3/callStack.js
@@ -44,3 +44,10 @@ function wrapValue(n) {
   let wrap2 = wrapValue(2);
   console.log(wrap1()); //1
   console.log(wrap2()); //2  
+
+  function multiplier(factor) {
+    return number => number * factor;
+  }
+  
+  let twice = multiplier(2);
+  console.log(twice(5)); //10
