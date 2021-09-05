34cd428ce97cc073c8795d2537920bc10e8f29f8
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-09-05 17:35:22 +0200

    Eloquent Javascript Introduction: Defining a Function 3

--- Eloquent Javascript/Chap3/definingFunction.js
+++ Eloquent Javascript/Chap3/definingFunction.js
@@ -19,3 +19,13 @@ const square = function(x) {
     }
     return result;
   };
+
+// Binding Scopes
+
+let x = 10;
+if (true) {
+  let y = 20;
+  var z = 30;
+  console.log(x + y + z); //60
+}
+console.log(x + z); //40
