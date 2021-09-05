15b3ae2ecc611f8121f7f0791325477264bcc2b3
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-09-05 18:05:06 +0200

    Eloquent Javascript Introduction: The call stack 2

--- Eloquent Javascript/Chap3/callStack.js
+++ Eloquent Javascript/Chap3/callStack.js
@@ -12,3 +12,11 @@ function green(who) {
 function square(x) {return x * x; }
 console.log(square(4, true, "hedgehog")); //16  
   
+
+function minus(a, b) {
+    if (b === undefined) return -a;
+    else return a -b;
+  }
+  
+  console.log(minus(10)); //-10
+  console.log(minus(10,5)); //5
