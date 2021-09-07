3f29611b3aeac10a4539cc1110a374a7cc23c45c
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-09-07 10:33:59 +0200

    Data Structures Chapter 5: Queues Priority Queues part1

--- Data Structures and Algorithms/Chapter 5 - Queues/usingQueues.js
+++ Data Structures and Algorithms/Chapter 5 - Queues/usingQueues.js
@@ -138,7 +138,24 @@ dispArray(nums);
 
 //priority Queues
 
-function Patient(name, code) {
+function Patieny(name, code) {
   this.name = name;
   this.code = code;
+  }
+function dequeue() {
+  let priority = this.dataStore[0].code;
+  for (let i = 1; i < this.dataStore.length; i++) {
+      if (this.dataStore[i].code < priority) {
+          priority = i;
+      }
+  }
+  return this.dataStore.splice(priority, 1);
+}
+function toString(){
+  let retStr = "";
+  for (let i = 0; i < this.dataStore.length; i++) {
+      retStr += this.dataStore[i].name + " code: "
+             + this.dataStore[i].code+ "\n";
+  }
+  return retStr; 
 }
