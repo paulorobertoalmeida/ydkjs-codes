7f8a7a709513adb2f668e2babc284e5fd139ca49
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-09-08 11:17:58 +0200

    Data Structures Chapter 7: Linked List List Class

--- Data Structures and Algorithms/Chapter6 - Linked List/objectBasedList.js
+++ Data Structures and Algorithms/Chapter6 - Linked List/objectBasedList.js
@@ -4,3 +4,13 @@ function Node(element) {
     this.element = element;
     this.next = null;
   }
+
+// Linked List Class
+
+function LList() {
+    this.head = new Node("head");
+    this.find = find;
+    this.insert = insert;
+    this.remove = remove;
+    this.display = display;
+  }  
