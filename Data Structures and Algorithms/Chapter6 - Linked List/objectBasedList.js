8dcffacb3986ab51eb0e9258aa3b82e526c9b159
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-09-08 12:00:41 +0200

    Data Structures Chapter 7: Linked List Final

--- Data Structures and Algorithms/Chapter6 - Linked List/objectBasedList.js
+++ Data Structures and Algorithms/Chapter6 - Linked List/objectBasedList.js
@@ -8,9 +8,59 @@ function Node(element) {
 // Linked List Class
 
 function LList() {
-    this.head = new Node("head");
-    this.find = find;
-    this.insert = insert;
-    this.remove = remove;
-    this.display = display;
-  }  
+  this.head = new Node("head");
+  this.find = find;
+  this.insert = insert;
+  this.findPrevious = findPrevious;
+  this.remove = remove;
+  this.display = display;
+}
+
+function remove(item) {
+  let prevNode = this.findPrevious(item);
+  if (!(prevNode.next == null)) {
+    prevNode.next = prevNode.next.next;
+  }
+}
+
+
+function findPrevious(item) {
+  let currNode = this.head;
+  while (!(currNode.next == null) &&
+          (currNode.next.element != item)) {
+            currNode = currNode.next
+          }
+          return currNode;
+}
+
+function display() {
+  let currNode = this.head;
+  while (!(currNode.next == null)) {
+    console.log(currNode.next.element);
+    currNode = currNode.next
+  }
+}
+function find(item) {
+  let current = this.head;
+  while (currNode.element != item) {
+    currNode = currNode.next;
+  }
+  return currNode
+}
+
+function insert(newElement, item) {
+  let newNode = new Node(newElement);
+  let current = this.find(item);
+  newNode.next = currNode.next;
+  current.next = newNode;
+}
+
+let cities = new LList();
+cities.insert("Conway", "head");
+cities.insert("Russellville", "Conway");
+cities.insert("Carlisle", "Russellville");
+cities.insert("Alma", "Carlisle");
+city.display();
+console.log();
+cities.remove("Carlisle");
+cities.display();
