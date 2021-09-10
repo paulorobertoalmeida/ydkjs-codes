424111256135378c78b098485d765814db1d1f47
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-09-10 12:30:04 +0200

    Repaso part 1

--- Eloquent Javascript/Chap4/objectsArrays.js
+++ Eloquent Javascript/Chap4/objectsArrays.js
@@ -63,3 +63,18 @@ console.log("left" in anObject);
 //false
 console.log("right" in anObject);
 //true
+
+console.log(Object.keys({x: 0, y: 0, z: 2}) ); //[ 'x', 'y', 'z' ]
+
+let objectA = {a: 1, b: 2};
+Object.assign(objectA, {b: 3, c: 4});
+console.log(objectA); //{ a: 1, b: 3, c: 4 }
+
+let journal = [
+    {events: ["work", "touched tree",       "pizza", "running", "television"],
+    squirrel: false},
+    {events: ["work", "ice cream", "cauliflower", "lasagna", "touched tree", "brushed teeth"],
+    squirrel: false},
+    {events: ["weekend", "cycling", "break", "peanuts", "beer"],
+    squirrel: true},
+  ];
---
+++ Repasso/repasoJs.js
@@ -0,0 +1,11 @@
+//Repaso Js
+
+alert("hello world");
+
+let name = "Paulo";
+let height = 182;
+let concat = name + " " + height;
+
+// document.write(name + " " + height + " ");
+let data = document.getElementById("data");
+data.innerHTML = concat
