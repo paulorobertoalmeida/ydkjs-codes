f0c143a4f28be0cd9e592dd1e548b4c395f1f14a
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-09-10 11:26:17 +0200

    Eloquent Javascript Chapter 4: Methods part 2

--- Eloquent Javascript/Chap4/objectsArrays.js
+++ Eloquent Javascript/Chap4/objectsArrays.js
@@ -22,3 +22,13 @@ console.log(typeof doh.toUpperCase);
 
 console.log(doh.toLocaleUpperCase());
 //DOH
+
+let sequence = [1,2,3];
+sequence.push(4);
+sequence.push(5);
+console.log(sequence);
+//[ 1, 2, 3, 4, 5 ]
+console.log(sequence.pop());
+//5
+console.log(sequence);
+//[ 1, 2, 3, 4 ]
