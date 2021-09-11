4475e4619323ceba80db099fb18dc13791619359
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-09-11 12:05:56 +0200

    Eloquent Javascript Chapter 4: Objects part 7

--- Eloquent Javascript/Chap4/objectsArrays.js
+++ Eloquent Javascript/Chap4/objectsArrays.js
@@ -107,3 +107,13 @@ function addEntry(events, squirrel) {
 addEntry(["work", "touched", "pizza", "running", "television"], false);
 addEntry(["work", "ice cream", "cauliflower", "lasagna", "touched tree", "brushed teeth"], false);
 addEntry(["weekend", "cycling", "break", "peanuts", "beer"], true);
+
+function phi(table) {
+    return (table[3] * table[0] - table[2] * table[1]) /
+    Math.sqrt((table[2] + table[3]) *
+              (table[0] + table[1]) *
+              (table[1] + table[3]) *
+              (table[2] + table[3]))
+  }
+  
+  console.log(phi([76, 9, 4, 1])); //0.27439773622801417
