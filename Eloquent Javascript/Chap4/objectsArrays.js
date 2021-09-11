e0593692f3cfabb3929532665fded0a784d30ddd
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-09-11 12:11:30 +0200

    Eloquent Javascript Chapter 4: Objects part 8

--- Eloquent Javascript/Chap4/objectsArrays.js
+++ Eloquent Javascript/Chap4/objectsArrays.js
@@ -117,3 +117,16 @@ function phi(table) {
   }
   
   console.log(phi([76, 9, 4, 1])); //0.27439773622801417
+
+  function tableFor(event, journal) {
+    let table = [0, 0, 0, 0];
+    for (let i = 0; i < journal.length; i++) {
+      let entry = journal[i], index = 0;
+      if (entry.event.includes(event)) index += 1;
+      if (entry.squirrel) index += 2;
+      table[index] += 1;
+    }
+    return table;
+  }
+  
+  console.log(tableFor("pizza", JOURNAL));
