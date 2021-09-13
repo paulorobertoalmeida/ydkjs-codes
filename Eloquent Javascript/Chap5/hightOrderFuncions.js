1010c99c3449d64c81a64dd1fb51e4c0a235961a
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-09-13 17:33:28 +0200

    Eloquent Js Chapter5: HOF part 6

--- Eloquent Javascript/Chap5/hightOrderFuncions.js
+++ Eloquent Javascript/Chap5/hightOrderFuncions.js
@@ -82,3 +82,18 @@ repeat(3, n => {
 
 ["A","B"].forEach(l => console.log(l)); //A, B  
 //l for letter
+
+
+//Filtering ARRAYS
+
+function filter(array, test) {
+    let passed = [];
+    for (let element of array) {
+        if (test(element)) {
+            passed.push(element);
+        }
+    }
+    return passed;
+}
+
+console.log(filter(SCRIPTS, script => script.living));
