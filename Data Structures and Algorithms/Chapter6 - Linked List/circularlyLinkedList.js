d6da740254885ba2cc339df0a02ec8655291c1a6
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-09-13 10:44:45 +0200

    Data Structures Chapter 6: Circularly List

---
+++ Data Structures and Algorithms/Chapter6 - Linked List/circularlyLinkedList.js
@@ -0,0 +1,9 @@
+function LList() {
+    this.head = new Node("head");
+    this.head.next = this.head;
+    this.find = find;
+    this.insert = insert;
+    this.display = display;
+    this.findPrevious = findPrevious;
+    this.remove = remove;
+}
