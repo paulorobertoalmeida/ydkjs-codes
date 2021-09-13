45815366f7ea289c26592ea213dc50e9bd4ca98a
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-09-13 17:41:14 +0200

    Eloquent Js Chapter5: HOF part 8

--- Eloquent Javascript/Chap5/hightOrderFuncions.js
+++ Eloquent Javascript/Chap5/hightOrderFuncions.js
@@ -111,3 +111,15 @@ function map(array, transform) {
 
 let rtlScript = SCRIPTS.filter(s => s.direction == "rtl");
 console.log(map(rtlScript, s => s.name));
+
+// Summarizing with reduce
+
+function reduce(array, combine, start) {
+    let current = start;
+    for (let element of array) {
+        current = combine(current, element);
+    }
+    return current;
+}
+
+console.log(reduce([1,2,3,4], (a,b) => a + b, 0)); //10
