e42a77df12199693ff9b0b3654bf7a0e1d7eae79
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-09-13 17:27:32 +0200

    Eloquent Js Chapter5: HOF part 4

--- Eloquent Javascript/Chap5/hightOrderFuncions.js
+++ Eloquent Javascript/Chap5/hightOrderFuncions.js
@@ -69,3 +69,12 @@ function greaterThan(n) {
 }
 let greaterThan10 = greaterThan(10);
 console.log(greaterThan10(11)); //true
+
+function unless(test, then) {
+    if (!test) then();
+}
+repeat(3, n => {
+    unless(n % 2 == 1, () => {
+        console.log(n, "is not even");
+    });
+});
