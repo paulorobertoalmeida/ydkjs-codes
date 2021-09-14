3647152ac951472d6718a40430994909ad3d1708
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-09-14 10:11:41 +0200

    Eloquent Js Chapter5: HOF part 10

--- Eloquent Javascript/Chap5/hightOrderFuncions.js
+++ Eloquent Javascript/Chap5/hightOrderFuncions.js
@@ -138,3 +138,31 @@ console.log(SCRIPTS.reduce((a,b) => {
 
 // COmposability
 
+//Exemple 1
+let biggest = null;
+for (let script of SCRIPTS) {
+  if (biggest == null ||
+      characterCount(biggest) < characterCount(script)) {
+        biggest = script;
+      }
+}
+console.log(biggest);
+
+//Exemple 2
+function average(array) {
+    return array.reduce((a,b) => a + b)/ array.length;
+  }
+  console.log(Math.round(average(SCRIPTS.filter(s => s.living).map(s => s.year))));
+  console.log(Math.round(average(SCRIPTS.filter(s => !s.living).map(s => s.year))));
+
+  // Exemple 3
+  let total = 0;
+  let count = 0;
+  for (let script of SCRIPTS) {
+    if (script.living) {
+      total += script.year;
+      count += 1;
+    }
+  }
+  
+  cocsole.log(Math.round(total /count));
