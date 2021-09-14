f448afbcc825ec6c109adfe41dc5a037f113607e
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-09-14 23:12:13 +0200

    Eloquent Js Chap6: Methods Part 2

--- Eloquent Javascript/Chap6/methods.js
+++ Eloquent Javascript/Chap6/methods.js
@@ -6,3 +6,13 @@ rabbit.speak = function(line) {
 };
 
 rabbit.speak("I'm alive"); //The rabbit says 'I'm alive'
+
+//Exemple 1
+
+function speak(line) {
+    console.log(`The '${thing.type} rabbit says '${line}'`);
+}
+let whiteRabbit = {type: "white", speak};
+let hungryRabbit = {type: "hungry", speak};
+
+whiteRabbit.speak("Oh my ears and whiskers, " + "how late its getting!");
