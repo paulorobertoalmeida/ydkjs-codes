79848bfb962b16a004e9a33c2b61590c999e8b94
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-09-14 17:41:02 +0200

    Data Structures Chapter 7: Dictionaries Part 2

--- Data Structures and Algorithms/Chapter 7 - Dictionaries/dictionaryClass.js
+++ Data Structures and Algorithms/Chapter 7 - Dictionaries/dictionaryClass.js
@@ -5,3 +5,16 @@ function Dictionary() {
 
 }
 
+function add(key, value) {
+    this.datastore[key] = value;
+}
+
+function find(key) {
+    delete this.datastore[key];
+}
+
+function showAll() {
+    for each (var key in Object.keys(this.datastore)) {
+        console.log(key + " -> " + this.datastore[key]);
+    }
+}
