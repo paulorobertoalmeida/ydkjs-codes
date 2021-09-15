3a1a75bd7782e526930edc16bcda1038298e4049
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-09-15 19:43:42 +0200

    Data Structures Chapter 7: Dictionaries Adding Sorting to the Dictionary Class Final

--- Data Structures and Algorithms/Chapter 7 - Dictionaries/sortingDictionary.js
+++ Data Structures and Algorithms/Chapter 7 - Dictionaries/sortingDictionary.js
@@ -6,3 +6,10 @@ a[1] = 'David';
 console.log(a);
 a.sort();
 console.log(a);
+
+// exelple 1
+function showAll() {
+    for each(var key in Object.keys(this.datastore).sort()) {
+        console.log(key + ' -> ' + this.datastore[key]);
+    }
+}
