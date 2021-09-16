cee1237cc2c3a31d4dfe69e539e074faf614f880
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-09-16 11:27:26 +0200

    Data Structures Chapter 8: Hashing Better Hashing par 2

--- Data Structures and Algorithms/Chapter 8 - Hashing/betterHashFunctions.js
+++ Data Structures and Algorithms/Chapter 8 - Hashing/betterHashFunctions.js
@@ -1,10 +1,44 @@
 
+function HashTable() {
+    this.table = new Array(137);
+    this.simpleHash = simpleHash;
+    this.betterHash = betterHash;
+    this.showDistro = showDistro;
+    this.put = put;
+    //this.get = get;
+}
+function put(data) {
+    var pos = this.simpleHash(data);
+    this.table[pos] = data;
+}
+
+function simpleHash(data) {
+    var total = 0;
+    for (var i = 0; i< data.length; i++) {
+        total += data.charCodeAt(i);
+    }
+    console.log("Hash value: " + data.length " -> " + total);
+    return total % this.table.length;
+}
+function showDistro(){
+    var n = 0;
+    for (var i = 0; i < this.table.length; i++) {
+        if (this.table[i]. != undefined) {
+            console.log(i + " : " + this.table[i]);
+        }
+    }
+}
+
+
 function betterHashFunctions(string, array) {
     const H = 37;
     var total = 0;
     for (var i = 0; i < string.length; i++) {
         total += H * total + string.charCodeAt(i);
     }
-    total = total % arr.length;
+    total = total % this.table.length;
+    if(total > 0) {
+        total += this.table.length-1;
+    }
     return parseInt(total);
 }
