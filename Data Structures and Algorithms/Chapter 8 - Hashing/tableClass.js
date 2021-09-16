fc81bb52b79efee0d845f4b4f9e195d5c9fb0184
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-09-16 10:58:01 +0200

    Data Structures Chapter 8: Hashing Hash Table Class Part 2

--- Data Structures and Algorithms/Chapter 8 - Hashing/tableClass.js
+++ Data Structures and Algorithms/Chapter 8 - Hashing/tableClass.js
@@ -1,9 +1,32 @@
 // A HAsh Table Class 
 
 function HashTable() {
-    this.table = new Array();
-    this.simplrHash = simplrHash;
+    this.table = new Array(137);
+    this.simpleHash = simpleHash;
     this.showDistro = showDistro;
     this.put = put;
     //this.get = get;
 }
+function put(data) {
+    var total = 0;
+    var pos = this.simpleHash(data);
+    this.table[pos] = data;
+}
+
+function simpleHash(data) {
+    var total = 0;for (var i = 0; i< data.length; i++) {
+        total += data.charCodeAt(i);
+    }
+    return total % this.table.length;
+}
+
+//Choosing a Hash Function
+
+function simpleHash(data) {
+    var total = 0;
+    for (let i = 0; i < data.length; i++) {
+        total += data.charCodeAt(i);
+    }
+    return total % this.table.length;
+}
+
