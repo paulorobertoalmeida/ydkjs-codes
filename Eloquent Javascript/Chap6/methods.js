a1475421df3868752df3a2a130bda74d97eb90e8
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-09-20 10:33:55 +0200

    Eloquent Js CHapter 6: Classes

--- Eloquent Javascript/Chap6/methods.js
+++ Eloquent Javascript/Chap6/methods.js
@@ -46,3 +46,21 @@ let killerRabbit = Object.create(protoRabbit);
 killerRabbit.type = "killer";
 killerRabbit.speak("SKREEEE!");
 //The killer rabbit says 'SKREEEE!'
+
+
+
+// CLASSES
+
+function makeRabbit(type) {
+  let rabbit = Object.create(protoRabbit);
+  rabbit.type = type;
+  return rabbit;
+}
+
+function Rabbit(type) {
+  this.type = type;
+}
+Rabbit.prototype.speak = function(line) {
+  console.log(`The ${this.type} rabbit says '${line}'`);
+};
+let weirdRabbit = new Rabbit("weird");
