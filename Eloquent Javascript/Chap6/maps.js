29260e0eb45a90ab415e61cec6ec278cdec6af82
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-09-20 14:28:47 +0200

    Eloquent Js CHapter 6: Maps Part 3

--- Eloquent Javascript/Chap6/maps.js
+++ Eloquent Javascript/Chap6/maps.js
@@ -11,3 +11,5 @@ console.log(`Julia is ${ages["Júlia"]}`); //Julia is 62
 console.log("Is Jack's age know?", "Jack" in ages); //Is Jack's age know? false
 
 console.log("Is toString's age know?", "toString" in ages); //Is toString's age know? true
+
+console.log("toString" in Object.create(null)); //false
