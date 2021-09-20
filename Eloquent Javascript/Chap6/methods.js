66547b7687f38d8f254aa9168d2992432f80968f
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-09-20 10:23:25 +0200

    Eloquent Js CHapter 6: Prototypes Part 4

--- Eloquent Javascript/Chap6/methods.js
+++ Eloquent Javascript/Chap6/methods.js
@@ -36,3 +36,13 @@ console.log(Object.getPrototypeOf(Object.prototype)); //null
 console.log(Object.getPrototypeOf(Math.max) == Function.prototype); // true
 console.log(Object.getPrototypeOf([]) == Array.prototype); //true
 
+let protoRabbit = {
+  speak(line) {
+    console.log(`The ${this.type} rabbit says '${line}'`);
+  }
+};
+
+let killerRabbit = Object.create(protoRabbit);
+killerRabbit.type = "killer";
+killerRabbit.speak("SKREEEE!");
+//The killer rabbit says 'SKREEEE!'
