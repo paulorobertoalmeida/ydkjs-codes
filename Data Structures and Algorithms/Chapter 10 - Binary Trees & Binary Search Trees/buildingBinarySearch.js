2e9e7346b096e145a82c626e15ae2166223ef495
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-09-24 22:02:06 +0200

    Data Structure Chap 10: Binary Trees and Binary Search Trees Exemple 2

--- Data Structures and Algorithms/Chapter 10 - Binary Trees & Binary Search Trees/buildingBinarySearch.js
+++ Data Structures and Algorithms/Chapter 10 - Binary Trees & Binary Search Trees/buildingBinarySearch.js
@@ -10,3 +10,50 @@ function Node(data, left, right) {
 function show() {
     return this.data;
 }
+
+// The BST and Node classes.
+
+function Node(data, left, right) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
+    this.show = show;
+}
+
+function show() {
+    return this.data;
+}
+
+function BTS() {
+    this.root = null;
+    this.insert = insert;
+    this.inOrder = inOrder;
+}
+
+function insert(data) {
+    let n = new Node(data, null, null);
+    if (this.root == null) {
+        this.root = n;
+    }
+    else {
+        let current = this.root;
+        let parent;
+        while (true) {
+            parent = current;
+            if (data < current.data) {
+                current = current.left;
+                if (current == null) {
+                    parent.left = n;
+                    break;
+                }
+            }
+            else {
+                current = current.right;
+                if (current == null) { 
+                    parent.right = n;
+                    break;
+                }
+            }
+        }
+    }
+}
