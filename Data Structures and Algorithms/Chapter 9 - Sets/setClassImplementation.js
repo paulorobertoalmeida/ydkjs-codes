02147a08cb9241c84f6123c8c39195212668f3c5
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-09-24 21:12:24 +0200

    Data Structure Chap 9: Sets Exemple 1

--- Data Structures and Algorithms/Chapter 9 - Sets/setClassImplementation.js
+++ Data Structures and Algorithms/Chapter 9 - Sets/setClassImplementation.js
@@ -34,3 +34,29 @@ function remove(data) {
         return false;
     }
 }
+
+// Using Set class
+
+let names = new Set();
+names.add("David");
+names.add("Jennifer");
+names.add("Cynthia");
+names.add("Mike");
+names.add("Raymond");
+if (names.add("Mike")) {
+  console.log("Mike added")
+} else {
+  console.log("Cant add Mike, must already be in set");
+}
+console.log(names.show());
+let removed = "Mike";
+if (names.remove(removed)) {
+  console.log(removed + " removed.");
+} else {
+  console.log(removed + " not removed");
+} names.add("Clayton");
+console.log(names.show());
+removed = "Alisa";
+if (names.remove("Mike")) {
+  console.log(remove + " remove.")
+}
