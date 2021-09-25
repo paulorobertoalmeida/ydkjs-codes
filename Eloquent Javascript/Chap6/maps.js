044d04a9d50eef647f4f9a5da1614ca0e3c4bec0
Paulo Almeida <62543673+paulorobertoalmeida@users.noreply.github.com>
2021-09-25 21:00:54 +0200

    part5

--- Eloquent Javascript/Chap6/maps.js
+++ Eloquent Javascript/Chap6/maps.js
@@ -36,3 +36,20 @@ Rabbit.prototype.toString = function() {
   return `a${this.type}rabbit`;
 };
 console.log(String(blackRabbit));
+
+//Symbols
+
+let sym = Symbol("name");
+console.log(sym == Symbol("name")); //false
+
+Rabbit.prototype[sym] = 55;
+console.log(blackRabbit[sym]);
+
+const toStringSymbol = Symbol("toString");
+Array.prototype[toStringSymbol] = function() {
+  return `${this.length} cm of blue yarn`;
+};
+
+console.log([1,2].toString()); //
+
+console.log([1,2][toStringSymbol]());
