1d8ea12a06c85c5c122c273991ddfc371b459423
Paulo Almeida <62543673+paulorobertoalmeida@users.noreply.github.com>
2021-09-25 17:33:22 +0200

    Binary Part 1

--- Data Structures and Algorithms/Chapter 10 - Binary Trees & Binary Search Trees/buildingBinarySearch.js
+++ Data Structures and Algorithms/Chapter 10 - Binary Trees & Binary Search Trees/buildingBinarySearch.js
@@ -57,3 +57,29 @@ function insert(data) {
         }
     }
 }
+
+// Here is the ocde for the inorde traversal functions:
+
+function inOrder(node) {
+    if (!(node == null)) {
+        inOrder(node.left);
+        console.log(node.show() + " ");
+        inOrder(node.right);
+    }
+}
+
+// Inorder traversal of a BST 
+
+let nums = new BST();
+nums.insert(23);
+nums.insert(45);
+nums.insert(16);
+nums.insert(37);
+nums.insert(3);
+nums.insert(99);
+nums.insert(22);
+console.log("Inorder traversal: ");
+inOrder(nums.root);
+
+//Preorder traversal function
+
