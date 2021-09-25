84313001800f7251b7f744593a1fee70abcd8d72
Paulo Almeida <62543673+paulorobertoalmeida@users.noreply.github.com>
2021-09-25 20:51:17 +0200

    part2

--- Eloquent Javascript/Chap6/maps.js
+++ Eloquent Javascript/Chap6/maps.js
@@ -13,3 +13,14 @@ console.log("Is Jack's age know?", "Jack" in ages); //Is Jack's age know? false
 console.log("Is toString's age know?", "toString" in ages); //Is toString's age know? true
 
 console.log("toString" in Object.create(null)); //false
+
+let ages = new Map();
+ages.set("Boris", 39);
+ages.set("Liang", 22);
+ages.set("Julia", 62);
+
+console.log(`Julia is ${ages.get("Julia")}`); //Julia is 62
+
+console.log("Is Jack's age known?", ages.has("Jack")); //s Jack's age known? false
+
+console.log(ages.has("toString")); //false
