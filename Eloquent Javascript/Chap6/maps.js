4c969c9067c6e6df0d024170bedecdb91f25709e
Paulo Almeida <62543673+paulorobertoalmeida@users.noreply.github.com>
2021-09-25 21:02:38 +0200

    part6

--- Eloquent Javascript/Chap6/maps.js
+++ Eloquent Javascript/Chap6/maps.js
@@ -50,6 +50,6 @@ Array.prototype[toStringSymbol] = function() {
   return `${this.length} cm of blue yarn`;
 };
 
-console.log([1,2].toString()); //
+console.log([1,2].toString()); //1,2
 
-console.log([1,2][toStringSymbol]());
+console.log([1,2][toStringSymbol]()); //2 cm of blue yarn
