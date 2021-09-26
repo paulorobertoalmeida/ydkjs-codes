0223fd1e756d921a8938302a62dd3f19c8c30b49
Paulo Almeida <62543673+paulorobertoalmeida@users.noreply.github.com>
2021-09-26 11:03:25 +0200

    Part 3

--- Eloquent Javascript/Chap6/maps.js
+++ Eloquent Javascript/Chap6/maps.js
@@ -88,3 +88,24 @@ class Matrix {
     this.content[y * this.width + x] = value;
   }
 }
+
+class MatrixIterator {
+  constructor(matrix) {
+    this.x = 0;
+    this.y = 0;
+    this.matrix = matrix;
+  }
+  next() {
+    if (this.y == this.matrix.height) return {done: true};
+
+    let value = { x: this.x,
+                  y: this.y,
+                  value: this.matrix.get(this.x, this.y)};
+    this.x++;
+    if (this.x == this.matrix.width) {
+      this.x = 0;
+      this.y++;
+    }
+    return {value, done: false}
+  }
+}
