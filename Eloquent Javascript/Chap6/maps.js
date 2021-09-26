dbf9b3d9cf80763aee8dbfc6dceedbd1b266783b
Paulo Almeida <62543673+paulorobertoalmeida@users.noreply.github.com>
2021-09-26 10:57:56 +0200

    Iterator Interface Part 2

--- Eloquent Javascript/Chap6/maps.js
+++ Eloquent Javascript/Chap6/maps.js
@@ -67,3 +67,24 @@ console.log(okIterator.next()); //{ value: 'O', done: false }
 console.log(okIterator.next()); // { value: 'k', done: false }
 
 console.log(okIterator.next()); //{ value: undefined, done: true }
+
+class Matrix {
+  constructor(width, height, element = (x,y) => undefined) {
+    this.width = width;
+    this.height = height;
+    this.content = [];
+
+    for (let y = 0; y < height; y++) {
+      for (let x = 0; x < width; x++) {
+        this.content[y * width + x] = element(x,y);
+      }
+    }
+  }
+
+  get(x,y) {
+    return this.content[y * this.width + x];
+  }
+  set(x, y, value) {
+    this.content[y * this.width + x] = value;
+  }
+}
