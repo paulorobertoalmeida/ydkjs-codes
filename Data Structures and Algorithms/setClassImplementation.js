57fa494495b8fe98a15c0ccef09a11d6c3430b49
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-09-29 09:24:37 +0200

    Data Structures Chapter 11: Graphs and Graph Algorithms part 10

--- Data Structures and Algorithms/setClassImplementation.js
+++ Data Structures and Algorithms/setClassImplementation.js
@@ -302,3 +302,22 @@ function showGraph() {
   }
 }
 
+function dfs(v) {
+  this.marked[v] = true;
+  if (this.adv[v] != undefined) {
+    console.log("Visited vertex: "+ v );
+  }
+  for each (var w in this.adj[v]) {
+    if (!this.marked[w]) {
+      this.dfs(w);
+    }
+  }
+}
+
+g = new Graph(5);
+g.addEdge(0.1);
+g.addEdge(0.2);
+g.addEdge(1.3);
+g.addEdge(2.4);
+g.showGraph();
+g.dfs(0);
