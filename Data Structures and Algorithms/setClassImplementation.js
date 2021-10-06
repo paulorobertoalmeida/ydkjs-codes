ecd1d31f51d98bc1d5bf331ff8bca5744a57f1be
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-10-06 08:37:21 +0200

    Data Sctructures Chapter 11: Implementing Topological Sorting Algo Part 4

--- Data Structures and Algorithms/setClassImplementation.js
+++ Data Structures and Algorithms/setClassImplementation.js
@@ -510,3 +510,37 @@ g.addEdge(0,1);
 g.vertexList = ["cs1, "cs2", "data structure", "assembly language", "operator systems", "algorithm"];
 g.showGraph();
 g.topSort();
+
+// Sorting Algorithms
+
+function CArray(numElements) {
+  this.dataStore = [];
+  this.pos = 0;
+  this.numElements = numElements;
+  this.insert = insert;
+  this.toString = toString;
+  this.clear = clear;
+  this.setData = setData;
+  this.swap = swap;
+
+  for (let i = 0; i < numElements; i++) {
+    this.dataStore[i] = i;
+  }
+}
+
+function setData() {
+  for (let i = 0; i < numElements; i++) {
+    this.dataStore[i] = Math.floor(Math.random() * (this.numElements+ i));
+  }
+}
+function setData() {
+  for (let i = 0; i < this.numElements; i++) {
+    this.dataStore[i] = Math.floorMath.floor(Math.random() * (this.numElements+1));
+  }
+}
+
+function clear() {
+  for (let i= 0; i < this.dataStore.length; i++) {
+    this.dataStore[i] = 0;
+  }
+}
