2986d8ad23804a48b8e26284331fa68c65de1d65
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-10-06 09:08:42 +0200

    Data Sctructures Chapter 11: Implementing Topological Sorting Algo Part 5

--- Data Structures and Algorithms/setClassImplementation.js
+++ Data Structures and Algorithms/setClassImplementation.js
@@ -535,7 +535,7 @@ function setData() {
 }
 function setData() {
   for (let i = 0; i < this.numElements; i++) {
-    this.dataStore[i] = Math.floorMath.floor(Math.random() * (this.numElements+1));
+    this.dataStore[i] = Math.floor(Math.random() * (this.numElements+1));
   }
 }
 
@@ -544,3 +544,29 @@ function clear() {
     this.dataStore[i] = 0;
   }
 }
+
+function insert(element) {
+  this.dataStore[this.pos++] = element;
+}
+
+function toString() {
+  let retstr= "";
+  for (let i = 0; i < this.dataStore.length; i++) {
+    retstr += this.dataStore[i] + " ";
+    if (i > 0 && i % 10 == 0) {
+      retstr += "\n";
+    }
+  }
+  return retstr;
+}
+
+function swap(arr, index1,index2) {
+  let temp = arr[index1];
+  arr[index1] = arr[index2];
+  arr[index2] = temp;
+}
+
+let numElements = 100;
+let myNums = new CArray(numElements);
+myNums.setData();
+console.log(myNums.toString());
