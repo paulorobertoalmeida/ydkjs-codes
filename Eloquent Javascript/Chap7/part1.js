71b0fab2e436ce1df07269fdd7a06c25a981c538
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-10-10 22:45:29 +0200

    Eloquent Javascript Chapter7: the taskpart 2

--- Eloquent Javascript/Chap7/part1.js
+++ Eloquent Javascript/Chap7/part1.js
@@ -37,3 +37,15 @@ class VillageState {
         }
     }
 }
+
+let first = new VillageState(
+    "Post Office",
+    [{place: "Post Office", address: "Alice's house"}]
+    );
+let next = first.move("Alice's House");
+
+console.log(next.place);
+
+console.log(next.parcels);
+
+console.log(first.place);
