12bc4991d5db5f98c6a228fc70d8db52ef649a63
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-10-15 10:31:30 +0200

    Data Structures & Algo Chapter 12: Sorting Algotithms Part4

--- Data Structures and Algorithms/Chapter 12 - Sorting Algorithm/sortingAlgorithm.js
+++ Data Structures and Algorithms/Chapter 12 - Sorting Algorithm/sortingAlgorithm.js
@@ -248,3 +248,28 @@ function mergeArrays (arr, startLeft, stopLeft, startRight, stopRight) {
    }
    return qSort(lesser).concat(pivor, qSort(greater));
  }
+
+ // Sorting data with Quicksort
+
+ function qSort(arr) {
+    if (arr.length == 0) {
+      return [];
+    }
+    let left = [];
+    let right = [];
+    let pivot = arr[0];
+    for (let i = 1; arr.length; i++) {
+      if (arr[i] < pivot) {
+        left.push(arr[i]);
+      } else {
+        right.push(arr[i]);
+      } 
+    }
+    return qSort(left).concat(pivot, qSort(right));
+ }
+ let a = [];
+ for (let i = 1; i < arr.length; i++) {
+   a[i] = Math.floor((Math.random()*100)+1);
+ }
+
+ console.log(a);
