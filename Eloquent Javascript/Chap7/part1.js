dac554d64e1d314eeb7b70a2c004dddcb0483f96
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-10-20 11:49:35 +0200

    Eloquent Javascript Chapter 7: Simulation

--- Eloquent Javascript/Chap7/part1.js
+++ Eloquent Javascript/Chap7/part1.js
@@ -56,3 +56,18 @@ let object = Object.freeze({value: 5});
 object.value = 10;
 console.log(object.value);
 // 5
+
+// Simulation.
+
+function runRobot(state, robot, memory) {
+    for (let turn = 0;; turn++) {
+        if (state.parcels.length === 0) {
+            console.log(`Done in ${turn} turn`);
+            break;
+        }
+        let action = robot(state, memory);
+        state = state.move(action.direction);
+        memory = action.memory;
+        console.log(`Moved to ${action.direction}`);
+    }
+}
