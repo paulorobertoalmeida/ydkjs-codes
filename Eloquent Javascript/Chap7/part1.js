b97324054103e1a984dc41290a40f102fa765cc8
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-10-21 12:21:55 +0200

    Eloquent Javascript Chapter 7: Simulation Parte 7

--- Eloquent Javascript/Chap7/part1.js
+++ Eloquent Javascript/Chap7/part1.js
@@ -112,3 +112,18 @@ function routeRobot(state, memory) {
     }
     return {direction: memory[0], memory: memory.slice(1)};
 }
+
+// Pathfinding.
+
+function findRoute(graph, from, to) {
+    let work = [{at: from, route: []}];
+    for (let i = 0; i < work.length; i++) {
+        let {at, route} = work[i];
+        for (let place of  graph[at]) {
+            if (place == to) return route.concat(place);
+            if (!work.some(w => w.at == place)){
+                work.push({at: place, route: route.concat(place)});
+            }
+        }
+    }
+}
