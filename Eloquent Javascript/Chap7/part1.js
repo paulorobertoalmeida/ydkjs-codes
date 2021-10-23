3fb262132f8c7af7a2565b0a29698015b287ca3b
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-10-23 23:46:26 +0200

    exemple 2.1

--- Eloquent Javascript/Chap7/part1.js
+++ Eloquent Javascript/Chap7/part1.js
@@ -127,3 +127,15 @@ function findRoute(graph, from, to) {
         }
     }
 }
+
+function goalOrientedRobot({place, parcels}, route) {
+    if (route.length == 0) {
+        let parcel = parcels[0];
+        if (parcel.place != place) {
+            route = findRoute(roadGraph, place, parcel.place);
+        } else {
+            route = findRoute(roadGraph, place, parcel.address);
+        }
+    }
+    return {direction: route[0], memory: route.slice(1)};
+}
