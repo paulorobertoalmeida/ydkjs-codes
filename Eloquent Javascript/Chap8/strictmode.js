85c44660ef966fd0ab5ca421810f004505eece86
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-10-25 10:08:38 +0200

    chap 8 part9

--- Eloquent Javascript/Chap8/strictmode.js
+++ Eloquent Javascript/Chap8/strictmode.js
@@ -35,3 +35,17 @@ test("dont convert case-less characters", () => {
 });
 
 //Debbuging.
+
+function numberToString(n, base = 10) {
+    let result = "", sign = "";
+    if (n < 0) {
+        sign = "-";
+        n = -n;
+    }
+    do {
+        result = String(n % base) + result;
+        n /= base;
+    } while (n > 0);
+    return sign + result;
+}
+console.log(numberToString(13, 10));
