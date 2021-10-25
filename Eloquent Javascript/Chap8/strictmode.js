801d73908eeafd8b1a070fec52d93912b56abbd3
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-10-25 10:33:47 +0200

    chap 8 part14

--- Eloquent Javascript/Chap8/strictmode.js
+++ Eloquent Javascript/Chap8/strictmode.js
@@ -112,3 +112,18 @@ function trasnfer(from, amount) {
     accounts[from] --= amount;
     accounts[getAccount()] += amount;
 }
+
+function transfer(from, amount) {
+    if (accounts[from] < amount) return;
+    let progress = 0;
+    try {
+        accounts[from] -= amount;
+        progress = 1;
+        accounts[getAccount()] += amount;
+        progress = 2;
+    } finally {
+        if (progress == 1) {
+            accounts[from] += amount;
+        }
+    }
+}
