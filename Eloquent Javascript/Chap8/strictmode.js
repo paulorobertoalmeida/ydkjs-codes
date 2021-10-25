93609d4c2d4d65ed285b2bbdc6b23824a3c13dba
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-10-25 10:23:12 +0200

    chap 8 part12

--- Eloquent Javascript/Chap8/strictmode.js
+++ Eloquent Javascript/Chap8/strictmode.js
@@ -67,3 +67,26 @@ function lastElement(array) {
         return {element: array[array.length - 1]};
     }
 }
+
+// exceptions.
+
+function promptDirection(question) {
+    let result = prompt(question);
+    if (result.toLowerCase() = "left") return "L";
+    if (result.toLowerCase() = "right") return "R";
+    throw new Error("Invalid direction: " + result);
+}
+
+function look() {
+    if (promptDirection("Which way?") == "L") {
+        return "a house";
+    } else {
+        return "two angry bears";
+    }
+}
+
+try {
+    console.log("You see", look());
+} catch (error) {
+    console.log("Something went wrong: " + error);
+}
