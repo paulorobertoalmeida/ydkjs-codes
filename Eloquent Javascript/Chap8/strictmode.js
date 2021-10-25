bfae38745d7312c855fb5f7975015880e5edc24b
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-10-25 08:50:13 +0200

    chap 8 part6

--- Eloquent Javascript/Chap8/strictmode.js
+++ Eloquent Javascript/Chap8/strictmode.js
@@ -20,3 +20,18 @@ let ferdinand = Person("Feratile"); // oops
 function goalOrientedRobot(state, memory) {
     //...
 }
+
+function test(label, body) {
+    if(!body()) console.log(`Failed: ${label}`);
+}
+test("convert Latin text to uppercase", () => {
+    return "hello".toUpperCase() == "HELLO";
+});
+test("convert Greek text to uppercase", () => {
+    return "Xaipete".toUpperCase() == "XAIPETE";
+});
+test("dont convert case-less characters", () => {
+    return "".toUpperCase() == "";
+});
+
+//Debbuging.
