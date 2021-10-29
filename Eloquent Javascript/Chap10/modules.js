8089ca5af0795f9f0aff318686460c1aa1993dc0
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-10-29 12:37:55 +0200

    Eloquent Javascript Chapter 10: Improvising Modules part2

--- Eloquent Javascript/Chap10/modules.js
+++ Eloquent Javascript/Chap10/modules.js
@@ -24,3 +24,17 @@ console.log(x);
 let plusOne = Function("n", "return n + 1");
 console.log(plusOne(4));
 
+// CommonJS.
+
+const ordinal = require("ordinal");
+const {days, months} = require("date-names");
+
+exports.formatDate = function(date, format) {
+    return format.replace(/YYYY|M(MMM)? |Do?|dddd/g, tag => {
+        if (tag == "M") return date.getMonth();
+        if (tag == "MMMM") return months[date.getMonth()];
+        if (tag =="D") return date.getDate();
+        if (tag == "Do") return ordinal(data.getDate(), format);
+        if (tag == "ddd") return days[date.getDay()];
+    });
+};
