74c676ced280197ac480d2341f524fb19400c617
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-10-29 12:43:19 +0200

    Eloquent Javascript Chapter 10: Improvising Modules part3

--- Eloquent Javascript/Chap10/modules.js
+++ Eloquent Javascript/Chap10/modules.js
@@ -38,3 +38,20 @@ exports.formatDate = function(date, format) {
         if (tag == "ddd") return days[date.getDay()];
     });
 };
+
+const {formatDate} = require("./format-date");
+
+console.log(formatDate(new Date(2019, 8, 13), "dddd the Do"));
+
+require.cache = Object.create(null);
+
+function require(name) {
+    if (!(name in require.cache)) {
+        let code = readFile(name);
+        let module = {exports: {}};
+        require.cache[name] = module;
+        let wrapper = Function("require, exports, module", code);
+        wrapper(require, module.exports, module);
+    }
+    return require.cache[name].exports;
+}
