5d65c90a04eb558154ccc001e38013a6d6b31706
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-11-04 06:48:46 -0300

    Eloquent Javascript Chapter 11: Callbacks part 5

--- Eloquent Javascript/Chap12/asynchronoysProgramming.js
+++ Eloquent Javascript/Chap12/asynchronoysProgramming.js
@@ -22,3 +22,15 @@ defineRequestType("note", (nest, content source, done) => {
     content();
 });
 
+// Promisses
+
+let fifteen = Promise.resolve(15);
+fifteen.then(value => console.log(`Got ${value}`));
+
+function storage(nest,name) {
+    return new Promise(resolve => {
+        nest.readStorage(name, result => resolve(result));
+    });
+}
+
+storage(bigOak, "enemies").then(value => console.log("Got", value));
