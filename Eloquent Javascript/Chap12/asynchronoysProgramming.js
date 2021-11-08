4b1044535206ff36df236d13461c6d6ef0aa7396
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-11-08 08:04:15 -0300

    Eloquent Javascript Chapter 11: Callbacks part 18

--- Eloquent Javascript/Chap12/asynchronoysProgramming.js
+++ Eloquent Javascript/Chap12/asynchronoysProgramming.js
@@ -153,3 +153,13 @@ function findRoute(from, to, connections) {
     }
     return null;
 }
+
+function routeRequest(nest, target, type, content) {
+    if (nest. neighbors.includes(target)) {
+        return request(nest, target, type, content);
+    } else {
+        let via = findRoute(nest.name, target, nest.state.connections);
+        if (!via) throw new Error(`No route to ${target}`);
+        return request
+    }
+}
