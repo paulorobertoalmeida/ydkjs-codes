b93dae50cd2ed283e0ddc82d6ff0f6a3cb3d1c3e
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-11-09 16:52:11 -0300

    Eloquent Javascript Chapter 11: Callbacks part 23

--- Eloquent Javascript/Chap12/asynchronoysProgramming.js
+++ Eloquent Javascript/Chap12/asynchronoysProgramming.js
@@ -175,6 +175,6 @@ requestType("storage", (nest, name) => storage(nest, name));
 function findInStorage(nest, name) {
     return storage(nest, name).then(found => {
         if (found != null) return found;
-        else
-    })
+        else return findInRemoteStorage(nest, name);
+    });
 }
