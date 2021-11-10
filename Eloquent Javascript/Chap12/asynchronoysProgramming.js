b6bedc35b5e028115a33134f046b6eefb0c6f6c8
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-11-10 09:07:00 -0300

    Eloquent Javascript Chapter 11: Callbacks part 26

--- Eloquent Javascript/Chap12/asynchronoysProgramming.js
+++ Eloquent Javascript/Chap12/asynchronoysProgramming.js
@@ -215,3 +215,16 @@ async function findInStorage(nest, name) {
     }
     throw new Error("Not Found");
 }
+
+// Generators
+
+function* powers(n) {
+    for (let current = n ;; current *= n) {
+        yield current;
+    }
+}
+
+for (let power of powers(3)) {
+    if (power > 50) break;
+    console.log(power);
+}
