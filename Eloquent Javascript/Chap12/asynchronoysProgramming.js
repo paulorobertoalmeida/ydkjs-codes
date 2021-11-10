79c61547ce521e5175e2b7251c0e4f3fdc3dbb98
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-11-10 09:04:37 -0300

    Eloquent Javascript Chapter 11: Callbacks part 25

--- Eloquent Javascript/Chap12/asynchronoysProgramming.js
+++ Eloquent Javascript/Chap12/asynchronoysProgramming.js
@@ -196,3 +196,22 @@ fucntion findInRemoteStorage(nest, name) {
     }
     return next();
 }
+
+//AsyncAwait
+
+async function findInStorage(nest, name) {
+    let local = await storage(nest, name);
+    if (local != null) return local;
+
+    let sources = network(nest).filter(n => n != nest.name);
+    while (sources.length > 0) {
+        let source = sources[Math.floor(Math.random() * sources.length)];
+        sources = sources.filter(n => n != source);
+
+        try {
+            let found = await routeRequest(nest, source, "storage", name);
+            if (found != null) return found;
+        } catch (_){}
+    }
+    throw new Error("Not Found");
+}
