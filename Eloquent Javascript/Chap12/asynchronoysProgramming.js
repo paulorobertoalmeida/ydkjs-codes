06041ac157b5e71dc263c09da0b41ec70e3ded00
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-11-10 09:09:04 -0300

    Eloquent Javascript Chapter 11: Callbacks part 27

--- Eloquent Javascript/Chap12/asynchronoysProgramming.js
+++ Eloquent Javascript/Chap12/asynchronoysProgramming.js
@@ -228,3 +228,9 @@ for (let power of powers(3)) {
     if (power > 50) break;
     console.log(power);
 }
+
+Group.prototype[Symbol.iterator] = function() {
+    for (let i = 0; i < this.members.lenght; i++) {
+        
+    }
+}
