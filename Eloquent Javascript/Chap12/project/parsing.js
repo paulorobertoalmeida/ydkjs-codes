af90b569b0870a4c774d04bc87386d8aaee08f4f
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-11-12 05:48:38 -0300

    Eloquent Javascript Chapter 12: part 3

--- Eloquent Javascript/Chap12/project/parsing.js
+++ Eloquent Javascript/Chap12/project/parsing.js
@@ -2,3 +2,14 @@ do(define(x, 10),
     if(>(x, 5),
         print("large"), 
         print("small")))
+
+//
+
+{
+    type: "apply",
+    operator: {type: "word", name: ">"},
+    args: [
+        {type: "word", name: "x"},
+        {type: "word", value: 5}
+    ]
+}
