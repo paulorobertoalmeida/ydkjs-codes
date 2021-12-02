747994256cf597581b1d0f856a7486ce716a6741
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-12-02 16:03:55 +0100

    even or Odd

---
+++ Kata Resorces/evenOrOdd.js
@@ -0,0 +1,9 @@
+function evenOrOdd(num) { //for string length take string
+    if (num % 2 == 0) {//and replace numm with string.length here
+      return "even";
+    } else {
+      return "odd";
+    }
+  }
+  
+  console.log(evenOrOdd(30));
