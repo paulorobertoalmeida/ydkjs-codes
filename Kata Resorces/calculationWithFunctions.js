4c814328bf519d88ff9b70a654e846fdf882269a
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-12-12 17:21:14 +0100

    Calculation with functions - Right One

--- Kata Resorces/calculationWithFunctions.js
+++ Kata Resorces/calculationWithFunctions.js
@@ -1,16 +1,39 @@
-const zero = (operation) => !operation ? 0 : operation(0);
-const one = (operation) => !operation ? 1 : operation(1);
-const two = (operation) => !operation ? 2 : operation(2);
-const three = (operation) => !operation ? 3 : operation(3);
-const four = (operation) => !operation ? 4 : operation(4);
-const five = (operation) => !operation ? 5 : operation(5);
-const six = (operation) => !operation ? 6 : operation(6);
-const seven = (operation) => !operation ? 7 : operation(7);
-const eight = (operation) => !operation ? 8 : operation(8);
-const nine = (operation) => !operation ? 9 : operation(9);
+//Right One
 
-const plus = (number) => (other_number) => other_number + number;
-const minus = (number) => (other_number) => other_number - number;
-const times = (number) => (other_number) => other_number * number;
-const dividedBy = (number) => (other_number) => other_number / number;
+function expression(number, operation){
+	if(!operation)
+		return number;
+	return operation(number);
+}
 
+function zero(operation) { return expression(0, operation); }
+function one(operation) { return expression(1, operation); }
+function two(operation) { return expression(2, operation); }
+function three(operation) { return expression(3, operation); }
+function four(operation) { return expression(4, operation); }
+function five(operation) { return expression(5, operation); }
+function six(operation) { return expression(6, operation); }
+function seven(operation) { return expression(7, operation); }
+function eight(operation) { return expression(8, operation); }
+function nine(operation) { return expression(9, operation); }
+
+function plus(x) {
+	return function(y) {
+		return y + x;
+	}
+}
+function minus(x) {
+	return function(y) {
+		return y - x;
+	}
+}
+function times(x) {
+	return function(y) {
+		return y * x;
+	}
+}
+function dividedBy(x) {
+	return function(y) {
+		return y / x;
+	}
+}
