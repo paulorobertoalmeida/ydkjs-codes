6023dd8356ef440ed22f87c4834521b473ce55dd
Paulo Almeida <62543673+paulorobertoalmeida@users.noreply.github.com>
2021-12-17 12:45:06 +0100

    Update repasoJs.js

--- Repasso/repasoJs.js
+++ Repasso/repasoJs.js
@@ -9,3 +9,10 @@ let concat = name + " " + height;
 // document.write(name + " " + height + " ");
 let data = document.getElementById("data");
 data.innerHTML = concat
+
+// Replace value in an array
+let list = [1, 2, 3, 7, 5, 6, 7, 8, 9];
+
+list[3] = 4;
+
+console.log(list);
