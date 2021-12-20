fc10cf0c48b32088fe795bf6720377ab3ec4092a
Paulo Almeida <62543673+paulorobertoalmeida@users.noreply.github.com>
2021-12-20 12:43:11 +0100

    Update arraymethods.js

--- Repasso/arraymethods.js
+++ Repasso/arraymethods.js
@@ -5,3 +5,8 @@ const arr = [5,6,7,8,9];
     console.log(item); // output: 6 7 8 9
   });
 
+// includes()
+  const arr = [1, 2, 3, 4, 5, 6];
+
+  arr.includes(2); // output: true
+  arr.includes(7); // output: false
