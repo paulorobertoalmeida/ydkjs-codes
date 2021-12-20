f9e9ac4fc0e0990a37b0f1f1c6f115fd0f4f29b3
Paulo Almeida <62543673+paulorobertoalmeida@users.noreply.github.com>
2021-12-20 12:53:21 +0100

    Update arraymethods.js

--- Repasso/arraymethods.js
+++ Repasso/arraymethods.js
@@ -19,3 +19,30 @@ const arr = [5,6,7,8,9];
   console.log(filtered); // output: [4, 5, 6]
 
   console.log(arr); // output: [1, 2, 3, 4, 5, 6]
+
+// map()
+
+  const arr = [1, 2, 3, 4, 5, 6];
+
+  // add one to every element
+  const oneAdded = arr.map(num => num + 1);
+  console.log(oneAdded); // output [2, 3, 4, 5, 6, 7]
+
+  console.log(arr); // output: [1, 2, 3, 4, 5, 6]
+
+// reduce()
+  const arr = [1, 2, 3, 4, 5, 6];
+
+  const sum = arr.reduce((total, value) => total + value, 0);
+
+// some()
+  const arr = [1, 2, 3, 4, 5, 6];
+
+  // at least one element is greater than 4?
+  const largeNum = arr.some(num => num > 4);
+  console.log(largeNum); // output: true
+
+  // at least one element is less than or equal to 0?
+  const smallNum = arr.some(num => num <= 0);
+  console.log(smallNum); // output: false
+  console.log(sum); // 21
