8df7a127b22d387e2b1915d1aced13f1062ef75c
Paulo Almeida <62543673+paulorobertoalmeida@users.noreply.github.com>
2021-12-20 12:46:08 +0100

    Update arraymethods.js

--- Repasso/arraymethods.js
+++ Repasso/arraymethods.js
@@ -10,3 +10,12 @@ const arr = [5,6,7,8,9];
 
   arr.includes(2); // output: true
   arr.includes(7); // output: false
+
+// filtered()
+  const arr = [1, 2, 3, 4, 5, 6];
+
+  // item(s) greater than 3
+  const filtered = arr.filter(num => num > 3);
+  console.log(filtered); // output: [4, 5, 6]
+
+  console.log(arr); // output: [1, 2, 3, 4, 5, 6]
