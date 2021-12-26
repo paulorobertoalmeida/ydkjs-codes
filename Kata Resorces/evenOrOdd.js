adcb7c39f1eb4269d6cd2bc4bfb8eeebcb3c1395
Paulo Almeida <62543673+paulorobertoalmeida@users.noreply.github.com>
2021-12-26 12:40:40 +0100

    Update evenOrOdd.js

--- Kata Resorces/evenOrOdd.js
+++ Kata Resorces/evenOrOdd.js
@@ -7,3 +7,13 @@ function evenOrOdd(num) { //for string length take string
   }
   
   console.log(evenOrOdd(30));
+
+
+// Another way to solve it
+
+
+const numbers = [1,2,3,4,5,6,7,8,9,10];
+
+const even = numbers.filter((el) => el % 2 == 0);
+
+console.log(even);
