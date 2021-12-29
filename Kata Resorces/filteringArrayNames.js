533638da943f5ee3d04644c1832d1303a7e2e03f
Paulo Almeida <62543673+paulorobertoalmeida@users.noreply.github.com>
2021-12-29 11:40:04 +0100

    Update filteringArrayNames.js

--- Kata Resorces/filteringArrayNames.js
+++ Kata Resorces/filteringArrayNames.js
@@ -77,3 +77,22 @@ console.log(peopleWhoBelongToTheIlluminati([
 //[ { name: 'Angelina Jolie', member: true },
 //  { name: 'Paris Hilton', member: true },
 //  { name: 'Bob Ziroll', member: true } ]
+
+
+// Make a filtered list of all the people who are old enough to see The Matrix (older than 18);
+
+function ofAge(arr){
+  const ageLimit = arr.filter((index) => index.age >= 18);
+  return ageLimit
+}
+// test
+console.log(ofAge([
+    { name: "Angelina Jolie", age: 80 },
+    { name: "Eric Jones", age: 2 },
+    { name: "Paris Hilton", age: 5 },
+    { name: "Kayne West", age: 16 },
+    { name: "Bob Ziroll", age: 100 }
+])); 
+// => 
+//[ { name: 'Angelina Jolie', age: 80 },
+//  { name: 'Bob Ziroll', age: 100 } ]
