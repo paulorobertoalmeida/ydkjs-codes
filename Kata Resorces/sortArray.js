d87672053b89419820d42cc2bf6ea7b5515e0582
Paulo Almeida <62543673+paulorobertoalmeida@users.noreply.github.com>
2021-12-29 12:13:31 +0100

    Update sortArray.js

--- Kata Resorces/sortArray.js
+++ Kata Resorces/sortArray.js
@@ -7,4 +7,13 @@ function leastToGreatest(arr) {
 
 console.log(leastToGreatest([1, 3, 5, 2, 90, 20])); // [1, 2, 3, 5, 20, 90] 
 
+// Sort an array from largest number to smallest
+
+function greatestToLeast(arr) {
+  const sortInvertOrder = arr.sort((b , a) => a - b);
+  return sortInvertOrder
+}
+
+console.log(greatestToLeast([1, 3, 5, 2, 90, 20])); // [90, 20, 5, 3, 2, 1] 
+
 
