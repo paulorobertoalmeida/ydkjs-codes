852df27bf9c9c1bdc382db17c7753fe9647f8013
Paulo Almeida <62543673+paulorobertoalmeida@users.noreply.github.com>
2021-12-29 11:24:13 +0100

    Update filteringArrayNames.js

--- Kata Resorces/filteringArrayNames.js
+++ Kata Resorces/filteringArrayNames.js
@@ -27,3 +27,15 @@ console.log(namesOnly([
     }
 ])); 
 // ["Angelina Jolie", "Eric Jones", "Paris Hilton", "Kayne West", "Bob Ziroll"]
+
+
+// Given an array of numbers, return a new array that has only the numbers that are 5 or greater.
+
+function fiveAndGreaterOnly(arr) {
+  const arrNum = arr.filter((index) => index >= 5);
+  return arrNum
+}
+// test
+console.log(fiveAndGreaterOnly([3, 6, 8, 2])); /// [6, 8]
+
+
