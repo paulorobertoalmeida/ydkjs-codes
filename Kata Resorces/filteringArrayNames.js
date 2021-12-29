df1413c73b15e43999bd331f5e0bf0196bed6e2b
Paulo Almeida <62543673+paulorobertoalmeida@users.noreply.github.com>
2021-12-29 11:35:04 +0100

    Update filteringArrayNames.js

--- Kata Resorces/filteringArrayNames.js
+++ Kata Resorces/filteringArrayNames.js
@@ -58,3 +58,22 @@ function fiveCharactersOrFewerOnly(arr) {
 // test
 console.log(fiveCharactersOrFewerOnly(["dog", "wolf", "by", "family", "eaten", "camping"])); // ["by", "dog", "wolf", "eaten"]
 
+
+// Given an array of people objects, return a new array that has filtered out all those who don't belong to the club.
+
+function peopleWhoBelongToTheIlluminati(arr){
+  const clubMembers = arr.filter((index) => index.member === true)
+  return clubMembers
+}
+// test
+console.log(peopleWhoBelongToTheIlluminati([
+    { name: "Angelina Jolie", member: true },
+    { name: "Eric Jones", member: false },
+    { name: "Paris Hilton", member: true },
+    { name: "Kayne West", member: false },
+    { name: "Bob Ziroll", member: true }
+]));
+// =>
+//[ { name: 'Angelina Jolie', member: true },
+//  { name: 'Paris Hilton', member: true },
+//  { name: 'Bob Ziroll', member: true } ]
