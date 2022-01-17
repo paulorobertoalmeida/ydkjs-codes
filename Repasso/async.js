66bc010410d7e26d5076883d855e04cb8c96bb17
Paulo Almeida <62543673+paulorobertoalmeida@users.noreply.github.com>
2022-01-17 18:50:48 +0100

    Create async.js

---
+++ Repasso/async.js
@@ -0,0 +1,14 @@
+const p = new Promise((res, rej) => {
+  res(1);
+})
+
+async function asyncReturn() {
+  return p;
+}
+
+function basicReturn() {
+  return Promise.resolve(p);
+}
+
+console.log(p === basicReturn()); // true
+console.log(p === asyncReturn()); // false
